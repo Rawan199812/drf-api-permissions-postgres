6e44d10d726941f054e80dcaea2e21bc417248c3
Rawan199812 <rawan.13579@icloud.com>
2021-06-22 02:19:37 +0300

    done

--- book/views.py
+++ book/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-
+from .permissions import IsAuthorOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 # Create your views here.
  
 class BooksList(generics.ListCreateAPIView):
+     permission_classes = (IsAuthenticatedOrReadOnly,)
+
      queryset = Book.objects.all()
      serializer_class = BookSerializer
 
 class BooksDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
--- books/settings.py
+++ books/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-vgyo_dcx&g@1x_!oe^wr!dv%1egef&4fg%0j#9^&^q0ps_8i&s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ALLOWED_HOSTS = ['0.0.0.0','localhost','127.0.0.1']
+
 
 
 # Application definition
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'books.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
